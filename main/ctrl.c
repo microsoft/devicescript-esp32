ad703ebfc517224305a64dcdf54c4babe9a338e6
Michal Moskal <michal@moskal.me>
2022-09-15 10:32:20 -0700

    update fw-id for different binaries; bump jd-c

--- main/ctrl.c
+++ main/ctrl.c
@@ -1,7 +1,17 @@
 #include "jdesp.h"
 
-const char app_dev_class_name[] = "ESP32 Jacscript 48 v0.2";
+#ifdef __XTENSA__
+#ifndef NO_JACSCRIPT
+const char app_dev_class_name[] = "ESP32-S2 Jacscript 48 v0.2";
 #define DEV_CLASS 0x3de1398b
+#else
+const char app_dev_class_name[] = "ESP32-S2 Cloud Connector 48 v0.2";
+#define DEV_CLASS 0x30a3c887
+#endif
+#else
+const char app_dev_class_name[] = "ESP32-C3 Jacscript";
+#define DEV_CLASS 0x33e239e5
+#endif
 
 uint32_t app_get_device_class(void) {
     return DEV_CLASS;
