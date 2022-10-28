67f9e8a1ec3da276d4b060f8cd5bf8052d028ac4
Michal Moskal <michal@moskal.me>
2022-10-27 17:02:46 -0700

    run sockets on thread

--- main/sock.c
+++ main/sock.c
@@ -14,6 +14,48 @@
 
 #define CHK_ERR(call) CHK(0 != (call))
 
+static xQueueHandle sock_cmds;
+static xQueueHandle sock_events;
+
+typedef struct {
+    unsigned ev;
+    const void *data;
+    unsigned size;
+} sock_event_t;
+
+typedef struct {
+    unsigned cmd;
+    union {
+        struct {
+            char *hostname;
+            int port;
+        } open;
+        struct {
+            uint8_t *data;
+            unsigned size;
+        } write;
+    };
+} sock_cmd_t;
+
+static void push_event(unsigned event, const void *data, unsigned size) {
+    sock_event_t evt = {
+        .ev = event,
+        .data = data,
+        .size = size,
+    };
+    xQueueSend(sock_events, &evt, 20);
+}
+
+static int sock_fd;
+static void raise_error(const char *msg) {
+    if (sock_fd) {
+        jd_tcpsock_close();
+        if (msg)
+            push_event(JD_CONN_EV_ERROR, msg, strlen(msg));
+        push_event(JD_CONN_EV_CLOSE, NULL, 0);
+    }
+}
+
 static int sock_create_and_connect(const char *hostname, const char *port_num) {
     struct addrinfo hints = {
         .ai_family = AF_UNSPEC,
@@ -24,6 +66,7 @@ static int sock_create_and_connect(const char *hostname, const char *port_num) {
     int s = getaddrinfo(hostname, port_num, &hints, &result);
     if (s) {
         LOG("getaddrinfo %s:%s: %d", hostname, port_num, s);
+        raise_error("can't resolve host");
         return -1;
     }
 
@@ -39,8 +82,10 @@ static int sock_create_and_connect(const char *hostname, const char *port_num) {
             break;
         }
 
-        if (rp->ai_next == NULL)
+        if (rp->ai_next == NULL) {
             LOG("connect %s:%s: %s", hostname, port_num, strerror(errno));
+            raise_error("can't connect");
+        }
 
         close(sfd);
     }
@@ -54,33 +99,15 @@ static int sock_create_and_connect(const char *hostname, const char *port_num) {
     return sockfd;
 }
 
-static int sock_fd;
-static void raise_error(const char *msg) {
-    if (sock_fd) {
-        jd_tcpsock_close();
-        if (msg)
-            jd_tcpsock_on_event(JD_CONN_EV_ERROR, msg, strlen(msg));
-        jd_tcpsock_on_event(JD_CONN_EV_CLOSE, NULL, 0);
-    }
-}
-
-int jd_tcpsock_new(const char *hostname, int port) {
+static void process_open(sock_cmd_t *cmd) {
     jd_tcpsock_close();
-    char *port_num = jd_sprintf_a("%d", port);
-    int r = sock_create_and_connect(hostname, port_num);
+    char *port_num = jd_sprintf_a("%d", cmd->open.port);
+    int r = sock_create_and_connect(cmd->open.hostname, port_num);
     jd_free(port_num);
+    jd_free(cmd->open.hostname);
     if (r > 0) {
         sock_fd = r;
-        jd_tcpsock_on_event(JD_CONN_EV_OPEN, NULL, 0);
-        return 0;
-    }
-    return -1;
-}
-
-void jd_tcpsock_close(void) {
-    if (sock_fd) {
-        close(sock_fd);
-        sock_fd = 0;
+        push_event(JD_CONN_EV_OPEN, NULL, 0);
     }
 }
 
@@ -97,18 +124,86 @@ static int forced_write(int fd, const void *buf, size_t nbytes) {
     return numread;
 }
 
+static void process_write(sock_cmd_t *cmd) {
+    unsigned size = cmd->write.size;
+    uint8_t *buf = cmd->write.data;
+    if (sock_fd) {
+        if (forced_write(sock_fd, buf, size) != (int)size)
+            raise_error("write error");
+    }
+    jd_free(buf);
+}
+
+void jd_tcpsock_close(void) {
+    if (sock_fd) {
+        close(sock_fd);
+        sock_fd = 0;
+    }
+}
+
+int jd_tcpsock_new(const char *hostname, int port) {
+    sock_cmd_t cmd = {
+        .cmd = JD_CONN_EV_OPEN,
+        .open = {.hostname = jd_strdup(hostname), .port = port},
+    };
+    if (xQueueSend(sock_cmds, &cmd, 20) == pdPASS) {
+        return 0;
+    } else {
+        jd_free(cmd.open.hostname);
+        return -1;
+    }
+}
+
 int jd_tcpsock_write(const void *buf, unsigned size) {
     if (!sock_fd)
         return -10;
-    // DMESG("wr %s", (const char*)buf);
-    if (forced_write(sock_fd, buf, size) == (int)size)
+
+    uint8_t *copy = jd_alloc(size);
+    memcpy(copy, buf, size);
+    sock_cmd_t cmd = {.cmd = JD_CONN_EV_MESSAGE, .write = {.data = copy, .size = size}};
+    if (xQueueSend(sock_cmds, &cmd, 0) == pdPASS) {
         return 0;
-    raise_error("write error");
-    return -1;
+    } else {
+        jd_free(copy);
+        return -1;
+    }
+}
+
+static void worker_main(void *arg) {
+    while (1) {
+        sock_cmd_t cmd;
+        if (!xQueueReceive(sock_cmds, &cmd, 20))
+            continue;
+        switch (cmd.cmd) {
+        case JD_CONN_EV_OPEN:
+            process_open(&cmd);
+            break;
+        case JD_CONN_EV_MESSAGE:
+            process_write(&cmd);
+            break;
+        default:
+            jd_panic();
+        }
+    }
+}
+
+void jd_sock_init(void) {
+    // The main task is at priority 1, so we're higher priority (run "more often").
+    // Timer task runs at much higher priority (~20).
+    unsigned stack_size = 4096;
+    sock_cmds = xQueueCreate(10, sizeof(sock_cmd_t));
+    sock_events = xQueueCreate(10, sizeof(sock_event_t));
+    TaskHandle_t task;
+    xTaskCreatePinnedToCore(worker_main, "tcpsock", stack_size, NULL, 2, &task, WORKER_CPU);
 }
 
 void jd_tcpsock_process(void) {
     static uint8_t sockbuf[128];
+
+    sock_event_t evt;
+    while (xQueueReceive(sock_events, &evt, 0)) {
+        jd_tcpsock_on_event(evt.ev, evt.data, evt.size);
+    }
 
     if (!sock_fd)
         return;
